8a6a98c30ae5d499c5e27ad021b487b080204dff
Luiz Henrique <luizhsou1@gmail.com>
2022-05-17 10:19:09 -0300

    added accordion

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-const tabmenu = document.querySelectorAll('.js-tabmenu li')
-const tabcontent = document.querySelectorAll('.js-tabcontent section')
+const ACTIVE_CLASS = 'active'
 
-if (tabmenu.length && tabcontent.length) {
-  tabcontent[0].classList.add('active')
+function initTabNav () {  
+  const tabmenu = document.querySelectorAll('.js-tabmenu li')
+  const tabcontent = document.querySelectorAll('.js-tabcontent section')
   
-  function activeTab(index) {
-    tabcontent.forEach(section => {
-      section.classList.remove('active')
+  if (tabmenu.length && tabcontent.length) {
+    tabcontent[0].classList.add(ACTIVE_CLASS)
+    
+    function activeTab(index) {
+      tabcontent.forEach(section => {
+        section.classList.remove(ACTIVE_CLASS)
+      })
+      tabcontent[index].classList.add(ACTIVE_CLASS)
+    }
+    
+    tabmenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index)
+      })
     })
-    tabcontent[index].classList.add('active')
   }
-  
-  tabmenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index)
-    })
-  })
 }
+
+function initAccordion () {
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+  
+  if (accordionList.length) {
+    function activeAccordion() {
+      this.classList.toggle(ACTIVE_CLASS)
+      this.nextElementSibling.classList.toggle(ACTIVE_CLASS)
+    }
+    
+    accordionList.forEach(item => {
+      item.addEventListener('click', activeAccordion)
+    })
+  }
+}
+
+initTabNav()
+initAccordion()
