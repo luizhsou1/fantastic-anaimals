151516216f87d2f6e6f72d7e120f596f62a08ce3
Luiz Henrique <luiz.henrique@zenvia.com>
2022-05-17 13:25:41 -0300

    added smoth scroll

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const ACTIVE_CLASS = 'active'
 
-function initTabNav () {  
+function initTabNav() {
   const tabmenu = document.querySelectorAll('.js-tabmenu li')
   const tabcontent = document.querySelectorAll('.js-tabcontent section')
-  
+
   if (tabmenu.length && tabcontent.length) {
     tabcontent[0].classList.add(ACTIVE_CLASS)
-    
+
     function activeTab(index) {
       tabcontent.forEach(section => {
         section.classList.remove(ACTIVE_CLASS)
       })
       tabcontent[index].classList.add(ACTIVE_CLASS)
     }
-    
+
     tabmenu.forEach((itemMenu, index) => {
       itemMenu.addEventListener('click', () => {
         activeTab(index)
@@ -22,20 +22,48 @@ function initTabNav () {
   }
 }
 
-function initAccordion () {
+function initAccordion() {
   const accordionList = document.querySelectorAll('.js-accordion dt')
-  
+
   if (accordionList.length) {
     function activeAccordion() {
       this.classList.toggle(ACTIVE_CLASS)
       this.nextElementSibling.classList.toggle(ACTIVE_CLASS)
     }
-    
+
     accordionList.forEach(item => {
       item.addEventListener('click', activeAccordion)
     })
   }
 }
 
+function initSmoothScroll() {
+  const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]')
+
+  if (internalLinks.length) {
+    function scrollToSection(event) {
+      event.preventDefault() // Previni que faça o comportamento de ir para a seção
+      const href = event.currentTarget.getAttribute('href')
+      const section = document.querySelector(href)
+
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+
+      // Forma alternativa
+      // window.scrollTo({
+      //   top: section.offsetTop,
+      //   behavior: 'smooth'
+      // })
+    }
+
+    internalLinks.forEach(link => {
+      link.addEventListener('click', scrollToSection)
+    })
+  }
+}
+
 initTabNav()
 initAccordion()
+initSmoothScroll()
