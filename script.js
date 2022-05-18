75f510b91febb43f82c0dc2d23161031442c62f8
Luiz Henrique <luiz.henrique@zenvia.com>
2022-05-18 13:55:01 -0300

    added scroll animation

--- script.js
+++ script.js
@@ -64,6 +64,29 @@ function initSmoothScroll() {
   }
 }
 
+function initScrollAnimation() {
+  const sections = document.querySelectorAll('.js-scroll')
+
+  if (sections.length) {
+    function scrollAnimation() {
+      sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top
+        const isVisibleSection = (sectionTop - (window.innerHeight * 0.8)) < 0
+        if (isVisibleSection) {
+          section.classList.add('active')
+        } else {
+          section.classList.remove('active')
+        }
+      })
+    }
+
+    window.addEventListener('scroll', scrollAnimation)
+
+    scrollAnimation()
+  }
+}
+
 initTabNav()
 initAccordion()
 initSmoothScroll()
+initScrollAnimation()
